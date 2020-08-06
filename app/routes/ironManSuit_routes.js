3de720dd14d4e35b161c3ae282ba3461e9b2ede5
danny-pantoja <danny.pantoja.23@gmail.com>
2020-08-06 10:37:53 -0700

    tested and seems back end is working

--- app/routes/ironManSuit_routes.js
+++ app/routes/ironManSuit_routes.js
@@ -27,67 +27,46 @@ router.get('/ironManSuit', requireToken, (req, res, next) => {
 // SHOW
 // GET
 router.get('/ironManSuit/:id', requireToken, (req, res, next) => {
-  // req.params.id will be set based on the `:id` in the route
   IronMansuit.findById(req.params.id)
     .then(handle404)
     .then(IronMansuit => res.status(200).json({ IronMansuit: IronMansuit.toObject() }))
     .catch(next)
 })
-
 // CREATE
-// POST /examples
+// POST
 router.post('/ironManSuit', requireToken, (req, res, next) => {
-  // set owner of new example to be current user
   req.body.ironManSuit.owner = req.user.id
 
   IronMansuit.create(req.body.ironManSuit)
-    // respond to succesful `create` with status 201 and JSON of new "example"
     .then(ironManSuit => {
       res.status(201).json({ ironManSuit: ironManSuit.toObject() })
     })
-    // if an error occurs, pass it off to our error handler
-    // the error handler needs the error message and the `res` object so that it
-    // can send an error message back to the client
     .catch(next)
 })
-
 // UPDATE
-// PATCH /examples/5a7db6c74d55bc51bdf39793
+// PATCH
 router.patch('/ironManSuit/:id', requireToken, removeBlanks, (req, res, next) => {
-  // if the client attempts to change the `owner` property by including a new
-  // owner, prevent that by deleting that key/value pair
   delete req.body.ironManSuit.owner
 
   IronMansuit.findById(req.params.id)
     .then(handle404)
     .then(ironManSuit => {
-      // pass the `req` object and the Mongoose record to `requireOwnership`
-      // it will throw an error if the current user isn't the owner
       requireOwnership(req, ironManSuit)
-
-      // pass the result of Mongoose's `.update` to the next `.then`
       return ironManSuit.updateOne(req.body.ironManSuit)
     })
-    // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
-
 // DESTROY
-// DELETE /examples/5a7db6c74d55bc51bdf39793
+// DELETE
 router.delete('/ironManSuit/:id', requireToken, (req, res, next) => {
   IronMansuit.findById(req.params.id)
     .then(handle404)
     .then(ironManSuit => {
-      // throw an error if current user doesn't own `example`
       requireOwnership(req, ironManSuit)
-      // delete the example ONLY IF the above didn't throw
       ironManSuit.deleteOne()
     })
-    // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
